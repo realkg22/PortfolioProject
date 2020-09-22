9258b177ea4727aa6ad2c30b9385a38484474a08
Kyle <kyl3gonzalez22@gmail.com>
2020-09-21 20:25:20 -0700

    Postgres DB and static root changes

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -75,12 +75,24 @@ WSGI_APPLICATION = 'personal_portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'projectportfolio',
+            'USER': 'kyle',
+            'PASSWORD': 'tamakyle22',
+            'HOST': 'localhost',
+            'PORT': '',
+        }
+    }
 
 
 # Password validation
