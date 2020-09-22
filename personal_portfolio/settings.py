32b6825a1ef3f065430de32d787db05a92f4a7e5
Kyle <kyl3gonzalez22@gmail.com>
2020-09-22 16:21:57 -0700

    Update settings.py

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'myproject',
         'USER': 'kyle',
-        'PASSWORD': 'tamakyle22',
+        'PASSWORD': 'djangoadmin123',
         'HOST': 'localhost',
         'PORT': '',
     }
